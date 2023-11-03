1271c0add0ab18edf281662373da98b4d71033b4
RAUDEL CASAS CUEVAS <raudel.casas4138@alumno.udg.mx>
2023-11-02 21:12:15 -0600

    Test of styles

--- src/Screens/Welcome.js
+++ src/Screens/Welcome.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import Spline from '@splinetool/react-spline';
+import './Welcome.css'
 
 export default function Welcome() {
   return (
     <div>
-        <Spline scene="https://prod.spline.design/qigPQzdTK7kWSHXR/scene.splinecode" />
-        <h1>¡Bienvenido!</h1>
+        <h1>METACALOR!</h1>
         <a href="/login">Iniciar sesión</a>
         <br />  
         <a href="/signup">Crear cuenta</a>
