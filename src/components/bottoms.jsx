8edcab0e4d21a0f1d33a5726bab48a754334248d
Sergiorey3124 <sergiorey3124@gmail.com>
2023-11-23 10:23:00 -0600

    Botones con Link

--- src/components/bottoms.jsx
+++ src/components/bottoms.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/count.css';
 import imagen1 from '../components/images/image2.png';
@@ -7,23 +8,24 @@ import imagen3 from '../components/images/platilloIcon.png';
 
 
 const Bottoms = () => {
-    return(
-        <div className="d-flex justify-content-center align-items-center barraBotones">
-            <a href="Report" className='reportes'>
-                <img className="position-absolute image1" width={'40px'} src={imagen1}/>
-                Generar reporte
-            </a>
-            <div className="linea-vertical"></div>
-            <a href='foodregister' className='calorias'>
-                <img class="position-absolute image1" width={'40px'} src={imagen2}/>
-                    Registrar calorias
-            </a>
-            <div className="linea-vertical2"></div>
-                <a href='FoodRecord' className='platillos'>
-                    <img class="position-absolute image1" width={'40px'} src={imagen3}/>
-                    Agregar platillo</a>
-        </div>
-    );
-}
+  return (
+    <div className="d-flex justify-content-center align-items-center barraBotones">
+      <Link to="/Report" className="reportes">
+        <img className="position-absolute image1" width="40px" src={imagen1} />
+        Generar reporte
+      </Link>
+      <div className="linea-vertical"></div>
+      <Link to="/foodregister" className="calorias">
+        <img className="position-absolute image1" width="40px" src={imagen2} />
+        Registrar calorias
+      </Link>
+      <div className="linea-vertical2"></div>
+      <Link to="/FoodRecord" className="platillos">
+        <img className="position-absolute image1" width="40px" src={imagen3} />
+        Agregar platillo
+      </Link>
+    </div>
+  );
+};
 
 export default Bottoms;
